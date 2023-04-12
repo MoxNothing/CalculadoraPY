1266bccfa854aca9b28fd42c148f62e684f7bc24
george <73075274@continental.edu.pe>
2023-04-12 15:58:08 -0500

    calFunciones

---
+++ calculadora.py
@@ -0,0 +1,24 @@
+print('Bienvenido a la calculadora ')
+
+def suma(x,y){
+        return x + y
+        }
+
+def resta(x,y){
+        return x-y
+
+        }
+def multi(x,y){
+        return x * y
+        }
+
+def division(x,y){
+        if(y == 0):
+            print('error al compilar')
+        else:
+        return x/y
+        }
+
+
+
+
